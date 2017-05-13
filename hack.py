fe7e78cc04196edab4082e82d00631de8dc43ca5
QuantumLiu <liuyiliang100@sina.com>
2017-05-13 15:00:05 +0800

    Three address now

--- hack.py
+++ hack.py
@@ -13,8 +13,8 @@ if win32api.GetSystemDefaultLangID()==2052:
     language=1#chinese
 else:
     language=0#english
-ver='0.2.0beta'
-btc_list=['https://btc.com/'+add for add in['13AM4VW2dhxYgXeQepoHkHSQuy6NgaEb94','12t9YDPgwueZ9NyMgw519p7AA8isjr6SMw']]
+ver='0.30beta'
+btc_list=['https://btc.com/'+add for add in['13AM4VW2dhxYgXeQepoHkHSQuy6NgaEb94','12t9YDPgwueZ9NyMgw519p7AA8isjr6SMw','115p7UMMngoj1pMvkpHijcRdfJNXj6LrLn']]
 reg=r'class="tx-item-summary-hash">(.*)</a>'
 r_list=[requests.get(btc) for btc in btc_list]
 print(u'请将在列出的比特币交易ID中选择一串，复制粘贴到勒索软件的Contact us,等待黑客验证后，点击check payment再点击Decypt即可恢复\n' if language else 'Please select one transaction ID，and paste it to the Contact us ,after the hacker confirmed,click Chek payment and then click Decrypt\n')
