792e66161f9e2ac3c12e9e9a80d3140cf836eb07
unnamed5719 <unnamed5719@users.noreply.github.com>
2017-05-13 23:46:23 -0500

    全版本兼容，适合刚安装Python的朋友
    Python2.7.13/3.6.1测试通过

--- hack.py
+++ hack.py
@@ -5,22 +5,36 @@ Created on Sat May 13 08:47:08 2017
 @author: Quantum Liu
 """
 
-import requests
+from __future__ import print_function
 import re
-import win32api
-import os
-if win32api.GetSystemDefaultLangID()==2052:
-    language=1#chinese
-else:
-    language=0#english
-ver='0.30beta'
-btc_list=['https://btc.com/'+add for add in['13AM4VW2dhxYgXeQepoHkHSQuy6NgaEb94','12t9YDPgwueZ9NyMgw519p7AA8isjr6SMw','115p7UMMngoj1pMvkpHijcRdfJNXj6LrLn']]
-reg=r'class="tx-item-summary-hash">(.*)</a>'
-r_list=[requests.get(btc) for btc in btc_list]
-print(u'请将在列出的比特币交易ID中选择一串，复制粘贴到勒索软件的Contact us,等待黑客验证后，点击check payment再点击Decypt即可恢复\n' if language else 'Please select one transaction ID，and paste it to the Contact us ,after the hacker confirmed,click Chek payment and then click Decrypt\n')
-for r in r_list:
-    for txid in re.findall(reg,r.text):
-        print(txid)
-        th=u'方法来自：https://www.zhihu.com/pin/846570592869183488#comment-280580859'
-print(th)
-os.system('pause')
+import sys
+
+ver = '0.31beta'
+
+address = [
+    '13AM4VW2dhxYgXeQepoHkHSQuy6NgaEb94',
+    '12t9YDPgwueZ9NyMgw519p7AA8isjr6SMw',
+    '115p7UMMngoj1pMvkpHijcRdfJNXj6LrLn']
+
+reg = r'class="tx-item-summary-hash">(.*)</a>'
+
+def downloader(url):
+    if sys.version[0] == '2':
+        import urllib2
+        r = urllib2.urlopen(url).read()
+        return r
+    else:
+        import urllib.request
+        r = urllib.request.urlopen(url).read().decode('utf-8')
+        return r
+
+for add in address:
+    btc = 'https://btc.com/'+add 
+    resp = downloader(btc)
+    txid = re.findall(reg, resp)
+    for tx in txid:
+        print(tx)
+
+print('\n请将在列出的比特币交易ID中选择一串，复制粘贴到勒索软件的Contact us,等待黑客验证后，点击check payment再点击Decypt即可恢复')
+print('Please select one transaction ID，and paste it to the Contact us ,after the hacker confirmed,click Chek payment and then click Decrypt\n')
+print('方法来自/form：https://www.zhihu.com/pin/846570592869183488')
